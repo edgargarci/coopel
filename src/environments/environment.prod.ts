a7a37564e5ba6b75768f4763a8248a58f3a61c95
Edgar Garcia Arrieta <edgarcia@grainchain.io>
2022-10-19 21:47:46 -0500

    configuration firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'employees-rinku',
+    appId: '1:866753504883:web:7205c68f5d0ab2533cac06',
+    databaseURL: 'https://employees-rinku-default-rtdb.firebaseio.com',
+    storageBucket: 'employees-rinku.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDNk6GBQzHXHGeqJaTavu_gVTmpqVvdr6s',
+    authDomain: 'employees-rinku.firebaseapp.com',
+    messagingSenderId: '866753504883',
+  },
   production: true
 };
